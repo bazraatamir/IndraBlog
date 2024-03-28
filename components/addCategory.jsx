392aa36394956cb90a6137172a1923e1b53b2f1e
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2024-03-28 16:43:18 +0800

    category page

--- components/addCategory.jsx
+++ components/addCategory.jsx
@@ -1,14 +1,37 @@
 "use client";
 import { useState } from "react";
+import axios from "axios";
 
 function AddCategories() {
   const [title, setTitle] = useState(null);
   const [description, setDescription] = useState();
+  const HandleClick = async () => {
+    let data = await axios.post("https://indra-blog.vercel.app/api/category", {
+      title,
+      description,
+    });
+    console.log(data);
+  };
   return (
     <div className=" flex flex-col gap-[40px]">
-      <input type="text" />
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-      <button className="bg-[#000] text-[#fff] ">add category</button>
+      <input
+        type="text"
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      ></textarea>
+      <button className="bg-[#000] text-[#fff] " onClick={HandleClick}>
+        add category
+      </button>
     </div>
   );
 }
